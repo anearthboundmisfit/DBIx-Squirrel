6a6ac3efce9363f241e32186c2b4a9813d34826e
An Earthbound Misfit <60038481+anearthboundmisfit@users.noreply.github.com>
2020-11-14 22:21:04 +0000

    Stubbed-out a utility module for common code

--- lib/DBIx/Squirrel/util.pm
+++ lib/DBIx/Squirrel/util.pm
@@ -4,15 +4,12 @@ use warnings;
 package DBIx::Squirrel::util;
 
 BEGIN {
-    *DBIx::Squirrel::util::VERSION     = *DBIx::Squirrel::VERSION;
+    require Exporter;
     @DBIx::Squirrel::util::ISA         = ( 'Exporter' );
-    %DBIx::Squirrel::util::EXPORT_TAGS = (
-        all => [
-            qw//
-        ]
-    );
+    *DBIx::Squirrel::util::VERSION     = *DBIx::Squirrel::VERSION;
+    %DBIx::Squirrel::util::EXPORT_TAGS = ( all => [ qw// ] );
     @DBIx::Squirrel::util::EXPORT_OK
-      = $DBIx::Squirrel::util::EXPORT_TAGS{ all };
+      = @{ $DBIx::Squirrel::util::EXPORT_TAGS{ all } };
 }
 
 1;
