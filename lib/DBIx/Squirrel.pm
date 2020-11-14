7bc27712a84ff86580d6f6eb840b506ba286252e
An Earthbound Misfit <60038481+anearthboundmisfit@users.noreply.github.com>
2020-11-14 22:23:31 +0000

    Stubbed-out main module with testable code

--- lib/DBIx/Squirrel.pm
+++ lib/DBIx/Squirrel.pm
@@ -4,16 +4,51 @@ use warnings;
 package DBIx::Squirrel;
 
 BEGIN {
-    $DBIx::Squirrel::VERSION = '2020.11.00';
     @DBIx::Squirrel::ISA     = ( 'DBI' );
+    $DBIx::Squirrel::VERSION = '2020.11.00';
 }
 
 use DBI;
 use DBIx::Squirrel::st;
 use DBIx::Squirrel::db;
+use Scalar::Util 'blessed';
+
+sub _is_db_handle {
+    my ( $maybe_dbh ) = @_;
+    if ( ref $maybe_dbh ) {
+        if ( blessed( $maybe_dbh ) && $maybe_dbh->isa( 'DBI::db' ) ) {
+            return 1;
+        }
+    }
+    return;
+}
+
+sub connect_cached {
+    my $handle = shift->DBI::connect_cached( @_ );
+    return $handle;
+}
 
 sub connect {
+    my $handle = do {
+        if ( @_ > 1 && _is_db_handle( $_[ 1 ] ) ) {
+            shift->connect_cloned( @_ );
+        } else {
+            shift->DBI::connect( @_ );
+        }
+    };
+    return $handle;
+}
 
+sub connect_cloned {
+    my $handle = do {
+        my ( $package, $master, $attr ) = @_;
+        if ( my $clone = $attr ? $master->clone( $attr ) : $master->clone ) {
+            bless $clone, join( '::', $package, 'db' );
+        } else {
+            undef;
+        }
+    };
+    return $handle;
 }
 
 1;
