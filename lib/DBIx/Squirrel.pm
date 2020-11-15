c2742ddbadbc0ca62102ce61500c84652f049972
An Earthbound Misfit <60038481+anearthboundmisfit@users.noreply.github.com>
2020-11-15 01:46:19 +0000

    Added some global configuration
    The following may be switched off if behaviour consistent with DBI's defaults is required:
    
    - relaxed parameter checking (named parameters)
    - active statements will automatically finish on execute

--- lib/DBIx/Squirrel.pm
+++ lib/DBIx/Squirrel.pm
@@ -6,8 +6,10 @@ package DBIx::Squirrel;
 ## no critic (TestingAndDebugging::ProhibitNoStrict)
 
 BEGIN {
-    @DBIx::Squirrel::ISA     = ( 'DBI' );
-    $DBIx::Squirrel::VERSION = '2020.11.00';
+    @DBIx::Squirrel::ISA                      = ( 'DBI' );
+    $DBIx::Squirrel::VERSION                  = '2020.11.00';
+    $DBIx::Squirrel::RELAXED_PARAM_CHECKS     = 1;
+    $DBIx::Squirrel::FINISH_ACTIVE_ON_EXECUTE = 1;
 }
 
 use DBI;
