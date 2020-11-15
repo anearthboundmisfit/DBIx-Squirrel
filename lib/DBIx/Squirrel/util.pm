a9190ef5264f3e4b91b49e117a1f3fea6688c762
An Earthbound Misfit <60038481+anearthboundmisfit@users.noreply.github.com>
2020-11-15 19:44:53 +0000

    Add a few things to the utility module
    - `throw`
    - `Dumper`

--- lib/DBIx/Squirrel/util.pm
+++ lib/DBIx/Squirrel/util.pm
@@ -9,9 +9,38 @@ BEGIN {
     require Exporter;
     @DBIx::Squirrel::util::ISA         = ( 'Exporter' );
     *DBIx::Squirrel::util::VERSION     = *DBIx::Squirrel::VERSION;
-    %DBIx::Squirrel::util::EXPORT_TAGS = ( all => [ qw// ] );
+    %DBIx::Squirrel::util::EXPORT_TAGS = (
+        all => [
+            qw/
+              Dumper
+              throw
+              /
+        ]
+    );
     @DBIx::Squirrel::util::EXPORT_OK
       = @{ $DBIx::Squirrel::util::EXPORT_TAGS{ all } };
+}
+
+use Carp;
+use Data::Dumper::Concise;
+
+sub throw {
+    @_ = do {
+        if ( @_ ) {
+            if ( @_ > 1 ) {
+                if ( my $format = shift ) {
+                    sprintf( $format, @_ );
+                } else {
+                    join( '', map { $_ // '' } @_ );
+                }
+            } else {
+                shift || 'Died';
+            }
+        } else {
+            $@ || $_ || 'Died';
+        }
+    };
+    goto &Carp::confess;
 }
 
 ## use critic
