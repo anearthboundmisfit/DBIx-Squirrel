bd1b8098881cc7dd83391cc73eaa032b017ebc71
An Earthbound Misfit <60038481+anearthboundmisfit@users.noreply.github.com>
2020-11-21 18:21:21 +0000

    Make "prepare" and "prepare_cached" more robust
    We're normalising statement placeholders to standard SQL ?-style placeholders but that behaviour is now optional.

--- lib/DBIx/Squirrel.pm
+++ lib/DBIx/Squirrel.pm
@@ -6,24 +6,27 @@ package DBIx::Squirrel;
 ## no critic (TestingAndDebugging::ProhibitNoStrict)
 
 BEGIN {
-    @DBIx::Squirrel::ISA                      = ( 'DBI' );
-    $DBIx::Squirrel::VERSION                  = '2020.11.00';
-    $DBIx::Squirrel::RELAXED_PARAM_CHECKS     = 0;
-    $DBIx::Squirrel::FINISH_ACTIVE_ON_EXECUTE = 1;
+    @DBIx::Squirrel::ISA     = ( 'DBI' );
+    $DBIx::Squirrel::VERSION = '2020.11.00';
 }
 
 use DBIx::Squirrel::util 'Dumper';
 use DBIx::Squirrel::dr;
 
+our $RELAXED_PARAM_CHECKS     = 0;
+our $FINISH_ACTIVE_ON_EXECUTE = 1;
+our $NORMALISED_STATEMENTS    = 1;
+
 BEGIN {
-    *connect_cached = *DBIx::Squirrel::dr::connect_cached;
-    *connect        = *DBIx::Squirrel::dr::connect;
-    *connect_clone  = *DBIx::Squirrel::dr::connect_clone;
-    *err            = *DBI::err;
-    *errstr         = *DBI::errstr;
-    *rows           = *DBI::rows;
-    *lasth          = *DBI::lasth;
-    *state          = *DBI::state;
+    *connect_cached        = *DBIx::Squirrel::dr::connect_cached;
+    *connect               = *DBIx::Squirrel::dr::connect;
+    *connect_clone         = *DBIx::Squirrel::dr::connect_clone;
+    *err                   = *DBI::err;
+    *errstr                = *DBI::errstr;
+    *rows                  = *DBI::rows;
+    *lasth                 = *DBI::lasth;
+    *state                 = *DBI::state;
+    *NORMALIZED_STATEMENTS = *NORMALISED_STATEMENTS;
 }
 
 ## use critic
