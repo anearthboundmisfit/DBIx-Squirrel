543ebd32cf02455698ef3dc353d806855f8a9639
An Earthbound Misfit <60038481+anearthboundmisfit@users.noreply.github.com>
2020-11-23 00:11:00 +0000

    Stubbed-out Result and ResultSet classes

---
+++ lib/DBIx/Squirrel/Result.pm
@@ -0,0 +1,16 @@
+use strict;
+use warnings;
+
+package DBIx::Squirrel::Result;
+
+## no critic (TestingAndDebugging::ProhibitNoStrict)
+
+BEGIN {
+    *DBIx::Squirrel::Result::VERSION = *DBIx::Squirrel::VERSION;
+}
+
+use namespace::autoclean;
+
+## use critic
+
+1;
